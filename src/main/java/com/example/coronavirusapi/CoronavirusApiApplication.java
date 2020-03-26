4005c637800b6d90e3fe16116da6109752f7264a
anjalia <anjalia@cs.uw.edu>
2020-03-26 11:04:37 -0700

    Adjusting application.properties to throw exception if no handler found. Allows for custom message response when requesting unsupported endpoint.

--- src/main/java/com/example/coronavirusapi/CoronavirusApiApplication.java
+++ src/main/java/com/example/coronavirusapi/CoronavirusApiApplication.java
@@ -3,14 +3,10 @@ package com.example.coronavirusapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class CoronavirusApiApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(CoronavirusApiApplication.class, args);
-
-        DispatcherServlet dispatcherServlet = (DispatcherServlet) context.getBean("dispatcherServlet");
-        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 }
--- src/main/java/com/example/coronavirusapi/CoronavirusApiControllerAdvice.java
+++ src/main/java/com/example/coronavirusapi/CoronavirusApiControllerAdvice.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@EnableWebMvc
 @ControllerAdvice
 public class CoronavirusApiControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(InvalidDateFormatException.class)
